a39d142d15f36eb34f08f624fc8082c3085e66dd
dnbworks <dooyongnsaakobalderama@gmail.com>
2021-12-27 15:22:16 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+temp = int(input("What is the temperature outside? "))
+
+if not(temp >= 0 and temp <= 30):
+    print("the temperature is bad outside")
+    print("stay outside")
+elif not(temp < 0 or temp > 30):
+    print("the temperature is good outside")
+    print("go outside")
